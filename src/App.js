14dce60b24f73071209a8b88b4b79c8c67964b60
Ethan Ikegami <ikeg225@berkeley.edu>
2022-05-14 22:24:04 -0700

    initial intro text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Header from './components/Header'
+import Header from './components/Header';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/Projects.js
@@ -0,0 +1,11 @@
+import styles from '../css/Projects.module.css';
+
+export default function Projects() {
+    return (
+        <div className={styles.frontline}>
+            <h2 className={styles.knock}>*knock knock*</h2>
+            <h1 className={styles.introBig}>Come on in to<br />Uyen's World!</h1>
+            <p className={styles.introText}>Uyen is a designer who loves to explore different styles and is never scared of coloring outside the lines!</p>
+        </div>
+    )
+}
