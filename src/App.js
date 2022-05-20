be4d5b8ce40db27be594ee37650224392060add5
Ethan Ikegami <ikeg225@berkeley.edu>
2022-05-20 16:20:17 -0700

    navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
+import Play from './components/Play';
+import About from './components/About';
 import Header from './components/Header';
 import Projects from './components/Projects';
+import ScrollToTop from './components/ScrollToTop';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header />
-      <Projects />
-    </div>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<Projects />} />
+          <Route path="/play" element={<Play />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </ScrollToTop>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,26 @@
 import logo from "../images/logo.png";
+import { NavLink }from 'react-router-dom';
 import linkedin from "../images/linkedin.png";
 import hamburger from "../images/hamburger.png";
 import instagram from "../images/instagram.png";
 import styles from "../css/Header.module.css";
 
 export default function Header() {
+    const activeLink = ({ isActive }) => isActive ? `${styles.activeLink}` : '';
+
     return (
         <header className={styles.top}>
             <div className={styles.leftHeader}>
-                <a href="#">projects</a>
-                <a href="#">play</a>
-                <a href="#">about</a>
+                <NavLink to="/" className={activeLink}>projects</NavLink>
+                <NavLink to="/play" className={activeLink}>play</NavLink>
+                <NavLink to="/about" className={activeLink}>about</NavLink>
             </div>
             <div className={styles.middleHeader}>
                 <img src={logo} width="50px"/>
             </div>
             <div className={styles.rightHeader}>
-                <a href="#"><img src={instagram} height="30px"/></a>
-                <a href="#"><img src={linkedin} height="30px"/></a>
+                <a href="https://www.instagram.com/pengwein/"><img src={instagram} height="30px" target="_blank" rel="noopener noreferrer"/></a>
+                <a href="https://www.linkedin.com/in/uyen-phan-3a4699236/" target="_blank" rel="noopener noreferrer"><img src={linkedin} height="30px"/></a>
             </div>
             <div className={styles.hamburger}>
                 <img src={hamburger} width="60px"/>
